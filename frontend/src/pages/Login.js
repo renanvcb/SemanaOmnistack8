1f2dcbcb7af24b5baba1bdbcfdc6beeab2d7830f
renanvcb <renanvcb@gmail.com>
2019-08-08 03:07:49 -0300

    Add existing file

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
 export default function login() {
+    const [username, setUsername] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(username);
+    }
+
     return (
-        <img src={logo} alt="Tindev" /> 
+        <div className="login-container">
+            <form onSubmit={handleSubmit}>
+                <img src={logo} alt="Tindev" />
+                <input
+                    placeholder="Digite seu usuário do GitHub."
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
     );
 }
