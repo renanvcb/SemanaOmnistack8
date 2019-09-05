f246cac6e2b6e5231073a33db4ad2d78ed038ae3
renanvcb <renanvcb@gmail.com>
2019-09-05 18:22:03 -0300

    Alteração de acessos no frontend. Retirando localhost

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -29,7 +29,7 @@ export default function Main({ match }) {
     }, [match.params.id]);
 
     useEffect(() => {
-        const socket = io('http://localhost:3333', {
+        const socket = io('http://192.168.10.134:3333', {
             query: { user: match.params.id }
         });
 
